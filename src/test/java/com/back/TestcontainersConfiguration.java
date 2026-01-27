d4102944c38167bcfda540f6c6b720a5ff1ac59b
NamSangwon <nsw3265@naver.com>
2026-01-27 15:08:29 +0900

    feat: DB 및 AI 설정

---
+++ src/test/java/com/back/TestcontainersConfiguration.java
@@ -0,0 +1,33 @@
+package com.back;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestcontainersConfiguration {
+    @Value("${DATABASE_USERNAME:faq}")
+    private String username;
+    @Value("${DATABASE_PASSWORD:faq123}")
+    private String password;
+    @Value("${DATABASE_NAME:faqdb}")
+    private String databaseName;
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgresContainer() {
+        return new PostgreSQLContainer(DockerImageName.parse("groonga/pgroonga:latest-alpine-18-slim"))
+                .withUsername(username)
+                .withPassword(password)
+                .withDatabaseName(databaseName);
+    }
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgreSQLContainer() {
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+    }
+}
